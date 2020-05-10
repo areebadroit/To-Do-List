cf63c751d153ba56cfa5d9e8a10949e31ebde264
areebadroit <mdareebansar@gmail.com>
2020-05-11 02:42:33 +0530

    To do

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+//check of specific tools by clicking
+//alert("connected");
+// $("li").click(function(){
+// 	$(this).toggleClass("completed");
+// });
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+// $("span").click(function(event){
+// 	$(this).parent().fadeOut(500,function(){
+// 		$(this).remove();
+// 	});
+// 	event.stopPropagation();
+// });
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+$("input[type='text']").keypress(function(){
+	if(event.which===13){
+		var todoText=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+ todoText +"</li>");
+	}
+});
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
